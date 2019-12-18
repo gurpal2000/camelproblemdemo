9d3a0eb15f1386f8502514b1823873e2c8f6829d
gurpal <g@gurpal.co.uk>
2019-12-18 01:19:10 +0000

    missed checkins

---
+++ src/main/java/com/example/camelproblemdemo/CamelproblemdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.camelproblemdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelproblemdemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CamelproblemdemoApplication.class, args);
+  }
+}
